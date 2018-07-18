3c7c886ccdb71bea935fb5a00e83f63fea817289
Md. Chonchol Mahmud <chonchol57@gmail.com>
2018-07-18 15:29:45 +0600

    code beautifier

--- wp-content/themes/unite-child/single-unite_flims.php
+++ wp-content/themes/unite-child/single-unite_flims.php
@@ -1,27 +1,27 @@
 <?php get_header(); ?>
-	<?php 
+	
+<?php 
 	$args = array( 'post_type' => 'unite_flims', 'posts_per_page' => 10 );
 	$loop = new WP_Query( $args );
 	while ( $loop->have_posts() ) : $loop->the_post(); 
-	?>
+?>
 	<h3><?php the_title(); ?></h3>
 	<div class="entry-content">
 			<p><?php the_content(); ?></p>
 	</div>
-<p><strong>Country:</strong>
-	<?php $terms = get_the_terms( $post->ID, 'country' );
-	foreach($terms as $term) { ;?>
-	   	<?php echo $term->name.','; ?>
-	<?php }; ?>
-</p>
-<p><strong>Genre:</strong>
-	<?php $terms = get_the_terms( $post->ID, 'genre' );
-	foreach($terms as $term) { ;?>
-	   	<?php echo $term->name.','; ?>
-	<?php }; ?>
-</p>
-<?php the_meta(); ?>	
+	<p><strong>Country:</strong>
+		<?php $terms = get_the_terms( $post->ID, 'country' );
+		foreach($terms as $term) { ;?>
+		   	<?php echo $term->name.','; ?>
+		<?php }; ?>
+	</p>
+	<p><strong>Genre:</strong>
+		<?php $terms = get_the_terms( $post->ID, 'genre' );
+		foreach($terms as $term) { ;?>
+		   	<?php echo $term->name.','; ?>
+		<?php }; ?>
+	</p>
+	<?php the_meta(); ?>	
 	<?php endwhile; ?>
-
 
 <?php get_footer(); ?>
