9d5de9bfcd16b0c81f62704dca32dd39f623e6c3
Md. Chonchol Mahmud <chonchol57@gmail.com>
2018-07-18 12:22:48 +0600

    create post type flims

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -3,4 +3,19 @@ add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
 function enqueue_child_theme_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
-?>
+
+
+
+function create_post_type() {
+  register_post_type( 'unite_flims',
+    array(
+      'labels' => array(
+        'name' => __( 'Flims' ),
+        'singular_name' => __( 'Flim' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+    )
+  );
+}
+add_action( 'init', 'create_post_type' );
